090bdfa272b3facaaeb15bc00fd17f957b8942b1
SuperDanny <jameslv@163.com>
2015-11-10 14:47:32 +0800

    增加全国地区邮政编码选取Demo
    在前人的基础上，完善改demo，并替换plist文件

---
+++ 全国地区及邮政编码plist/AreaPicker/AreaPicker/AreaPicker/DDAreaPickerView.m
@@ -0,0 +1,234 @@
+//
+//  DDAreaPickerView.m
+//  AreaPicker
+//
+//  Created by SuperDanny on 15/11/10.
+//  Copyright © 2015年 SuperDanny. All rights reserved.
+//
+
+#import "DDAreaPickerView.h"
+#import <QuartzCore/QuartzCore.h>
+
+#define kDuration 0.3
+
+@interface DDAreaPickerView () {
+    NSArray *provinces, *cities, *areas;
+}
+
+@end
+
+@implementation DDAreaPickerView
+
+@synthesize delegate    = _delegate;
+@synthesize datasource  = _datasource;
+@synthesize pickerStyle = _pickerStyle;
+@synthesize locate      = _locate;
+
+- (void)dealloc {
+    self.datasource = nil;
+    self.delegate = nil;
+}
+
+- (DDLocation *)locate {
+    if (_locate == nil) {
+        _locate = [[DDLocation alloc] init];
+    }
+    
+    return _locate;
+}
+
+- (id)initWithStyle:(DDAreaPickerStyle)pickerStyle withDelegate:(id <DDAreaPickerDelegate>)delegate andDatasource:(id <DDAreaPickerDatasource>)datasource {
+    
+    self = [[[NSBundle mainBundle] loadNibNamed:@"DDAreaPickerView" owner:self options:nil] objectAtIndex:0];
+    if (self) {
+        self.delegate                = delegate;
+        self.pickerStyle             = pickerStyle;
+        self.datasource              = datasource;
+        self.locatePicker.dataSource = self;
+        self.locatePicker.delegate   = self;
+        
+        provinces = [self.datasource areaPickerData:self];
+        cities = provinces[0][@"sub"];
+        
+        self.locate.state = provinces[0][@"name"];
+        self.locate.zipCode = cities[0][@"zipcode"];
+        
+        if (self.pickerStyle == DDAreaPickerWithStateAndCityAndDistrict) {
+            self.locate.city = cities[0][@"name"];
+            
+            areas = cities[0][@"sub"];
+            if (areas.count > 0) {
+                self.locate.district = areas[0];
+            } else {
+                self.locate.district = @"";
+            }
+            
+        } else {
+            self.locate.city = cities[0][@"name"];
+        }
+    }
+        
+    return self;
+    
+}
+
+#pragma mark - PickerView lifecycle
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    if (self.pickerStyle == DDAreaPickerWithStateAndCityAndDistrict) {
+        return 3;
+    } else {
+        return 2;
+    }
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    switch (component) {
+        case 0:
+            return [provinces count];
+            break;
+        case 1:
+            return [cities count];
+            break;
+        case 2:
+            if (self.pickerStyle == DDAreaPickerWithStateAndCityAndDistrict) {
+                return [areas count];
+                break;
+            }
+        default:
+            return 0;
+            break;
+    }
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (self.pickerStyle == DDAreaPickerWithStateAndCityAndDistrict) {
+        switch (component) {
+            case 0:
+                return provinces[row][@"name"];
+                break;
+            case 1:
+                return cities[row][@"name"];
+                break;
+            case 2:
+                if ([areas count] > 0) {
+                    return areas[row];
+                    break;
+                }
+            default:
+                return  @"";
+                break;
+        }
+    } else {
+        switch (component) {
+            case 0:
+                return provinces[row][@"name"];
+                break;
+            case 1:
+                return cities[row][@"name"];
+                break;
+            default:
+                return @"";
+                break;
+        }
+    }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    if (self.pickerStyle == DDAreaPickerWithStateAndCityAndDistrict) {
+        switch (component) {
+            case 0:
+                cities = provinces[row][@"sub"];
+                [self.locatePicker selectRow:0 inComponent:1 animated:YES];
+                [self.locatePicker reloadComponent:1];
+                
+                areas = cities[0][@"sub"];
+                [self.locatePicker selectRow:0 inComponent:2 animated:YES];
+                [self.locatePicker reloadComponent:2];
+                
+                self.locate.state = provinces[row][@"name"];
+                self.locate.city = cities[0][@"name"];
+                if ([areas count] > 0) {
+                    self.locate.district = areas[0];
+                } else {
+                    self.locate.district = @"";
+                }
+                break;
+            case 1:
+                areas = cities[row][@"sub"];
+                [self.locatePicker selectRow:0 inComponent:2 animated:YES];
+                [self.locatePicker reloadComponent:2];
+                
+                self.locate.city = cities[row][@"name"];
+                self.locate.zipCode = cities[row][@"zipcode"];
+                if ([areas count] > 0) {
+                    self.locate.district = areas[0];
+                } else {
+                    self.locate.district = @"";
+                }
+                break;
+            case 2:
+                if ([areas count] > 0) {
+                    self.locate.district = areas[row];
+                } else{
+                    self.locate.district = @"";
+                }
+                break;
+            default:
+                break;
+        }
+    } else {
+        switch (component) {
+            case 0:
+                cities = provinces[row][@"sub"];
+                [self.locatePicker selectRow:0 inComponent:1 animated:YES];
+                [self.locatePicker reloadComponent:1];
+                
+                self.locate.state = provinces[row][@"name"];
+                self.locate.city = cities[0][@"name"];
+                self.locate.zipCode = cities[0][@"zipcode"];
+                break;
+            case 1:
+                self.locate.city = cities[row][@"name"];
+                self.locate.zipCode = cities[row][@"zipcode"];
+                break;
+            default:
+                break;
+        }
+    }
+    
+    if([self.delegate respondsToSelector:@selector(pickerDidChaneStatus:)]) {
+        [self.delegate pickerDidChaneStatus:self];
+    }
+
+}
+
+
+#pragma mark - animation
+
+- (void)showInView:(UIView *)view {
+    CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
+    self.frame = CGRectMake(0, view.frame.size.height, screenWidth, self.frame.size.height);
+    [view addSubview:self];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        self.frame = CGRectMake(0, view.frame.size.height - self.frame.size.height, screenWidth, self.frame.size.height);
+    }];
+    
+}
+
+- (void)cancelPicker {
+    CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
+
+    [UIView animateWithDuration:0.3
+                     animations:^{
+                         self.frame = CGRectMake(0, self.frame.origin.y+self.frame.size.height, screenWidth, self.frame.size.height);
+                     }
+                     completion:^(BOOL finished){
+                         [self removeFromSuperview];
+                         
+                     }];
+    
+}
+
+@end
---
+++ 全国地区及邮政编码plist/AreaPicker/AreaPicker/ViewController.m
@@ -0,0 +1,81 @@
+//
+//  ViewController.m
+//  AreaPicker
+//
+//  Created by SuperDanny on 15/11/10.
+//  Copyright © 2015年 SuperDanny. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "DDAreaPickerView.h"
+
+@interface ViewController () <UITextFieldDelegate, DDAreaPickerDelegate, DDAreaPickerDatasource>
+@property (strong, nonatomic) DDAreaPickerView *locatePicker;
+
+-(void)cancelLocatePicker;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIButton *areaBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    areaBtn.frame = CGRectMake(100, 50, 100, 40);
+    areaBtn.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:areaBtn];
+    [areaBtn addTarget:self action:@selector(showAreaPicker) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIButton *cityBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    cityBtn.frame = CGRectMake(100, CGRectGetMaxY(areaBtn.frame)+50, 100, 40);
+    cityBtn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:cityBtn];
+    [cityBtn addTarget:self action:@selector(showCityPicker) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)showAreaPicker {
+    [self cancelLocatePicker];
+    self.locatePicker = [[DDAreaPickerView alloc] initWithStyle:DDAreaPickerWithStateAndCityAndDistrict
+                                                   withDelegate:self
+                                                  andDatasource:self];
+    [self.locatePicker showInView:self.view];
+}
+
+- (void)showCityPicker {
+    [self cancelLocatePicker];
+    self.locatePicker = [[DDAreaPickerView alloc] initWithStyle:DDAreaPickerWithStateAndCity
+                                                   withDelegate:self
+                                                  andDatasource:self];
+    [self.locatePicker showInView:self.view];
+}
+
+#pragma mark - DDAreaPicker delegate
+-(void)pickerDidChaneStatus:(DDAreaPickerView *)picker
+{
+    if (picker.pickerStyle == DDAreaPickerWithStateAndCityAndDistrict) {
+        NSLog(@"省、市、区、邮政编码：%@", [NSString stringWithFormat:@"%@ %@ %@ %@", picker.locate.state, picker.locate.city, picker.locate.district, picker.locate.zipCode]);
+    } else{
+        NSLog(@"省、市、邮政编码：%@", [NSString stringWithFormat:@"%@ %@ %@", picker.locate.state, picker.locate.city, picker.locate.zipCode]);
+    }
+}
+
+-(NSArray *)areaPickerData:(DDAreaPickerView *)picker
+{
+    NSArray *data = [[NSDictionary alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"address.plist" ofType:nil]][@"address"];
+    return data;
+}
+
+-(void)cancelLocatePicker
+{
+    [self.locatePicker cancelPicker];
+    self.locatePicker.delegate = nil;
+    self.locatePicker = nil;
+}
+
+#pragma mark -
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
