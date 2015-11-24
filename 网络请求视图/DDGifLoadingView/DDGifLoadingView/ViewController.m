bc24d8d981ec2e4796e202b0718d948f6d41013b
SuperDanny <jameslv@163.com>
2015-11-25 00:23:13 +0800

    增加网络请求视图

---
+++ 网络请求视图/DDGifLoadingView/DDGifLoadingView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DDGifLoadingView
+//
+//  Created by SuperDanny on 15/11/14.
+//  Copyright © 2015年 SuperDanny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
