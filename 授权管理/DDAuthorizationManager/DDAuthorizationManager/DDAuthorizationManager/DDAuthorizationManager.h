53c99d9f4630a6ce9c56db52bdb852dc287f4245
SuperDanny <jameslv@163.com>
2016-03-22 14:18:34 +0800

    update

---
+++ 授权管理/DDAuthorizationManager/DDAuthorizationManager/DDAuthorizationManager/DDAuthorizationManager.h
@@ -0,0 +1,13 @@
+//
+//  DDAuthorizationManager.h
+//  DDAuthorizationManager
+//
+//  Created by SuperDanny on 16/3/11.
+//  Copyright © 2016年 SuperDanny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDAuthorizationManager : NSObject
+
+@end
