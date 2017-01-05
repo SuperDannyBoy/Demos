5e1684ff400c60570dc7a5ebaff180f520794c7b
SuperDanny <jameslv@163.com>
2017-01-05 16:09:19 +0800

    add DDAlertController
    add DDAlertController

---
+++ DD系列/DDAlertController/DDAlertController.h
@@ -0,0 +1,23 @@
+//
+//  DDAlertController.h
+//  NightclubLive
+//
+//  Created by SuperDanny on 2017/1/5.
+//  Copyright © 2017年 李善忠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAlertAction : UIAlertAction
+
+@property (nonatomic,strong) UIColor *textColor; /**< 按钮title字体颜色 */
+
+@end
+
+@interface DDAlertController : UIAlertController
+
+@property (nonatomic,strong) UIColor *tintColor; /**< 统一按钮样式 不写系统默认的蓝色 */
+@property (nonatomic,strong) UIColor *titleColor; /**< 标题的颜色 */
+@property (nonatomic,strong) UIColor *messageColor; /**< 信息的颜色 */
+
+@end
